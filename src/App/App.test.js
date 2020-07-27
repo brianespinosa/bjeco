d74d21da7b6248660a3ecfc094ee10d1ae1b065a
Brian Espinosa <b@bje.co>
2020-07-26 18:49:13 -0700

    Fix test

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+test('Renders a splash logo', () => {
+  const { getByRole } = render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+  const linkElement = getByRole(/img/i);
   expect(linkElement).toBeInTheDocument();
 });
