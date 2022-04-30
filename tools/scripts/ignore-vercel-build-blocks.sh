6ec9abdd391be85713a0c64c2ddb4f4ff375be31
Brian Espinosa <b@bje.co>
2022-04-29 17:54:19 -0700

    Fix script to check for changed libs, not apps

--- tools/scripts/ignore-vercel-build-blocks.sh
+++ tools/scripts/ignore-vercel-build-blocks.sh
@@ -12,7 +12,7 @@ TS_VERSION=$(node -e "console.log(require('./package.json').devDependencies['typ
 # SHOULD NOT NEED THE ABOVE STEPS AS WE HAVE ZERO INSTALLS ENABLED WITH YARN
 
 # Run the affected command, comparing latest commit to the one before that
-npx nx affected:apps --plain --base HEAD~1 --head HEAD | grep $APP -q
+npx nx affected:libs --plain --base HEAD~1 --head HEAD | grep $APP -q
 
 # Store result of the previous command (grep)
 IS_AFFECTED=$?
