af541bdce8b8d5d5fec4cd93222614105045f1cb
Brian Espinosa <b@bje.co>
2022-04-30 18:23:32 -0700

    Tests

--- libs/blocks/src/Title/Title.spec.tsx
+++ libs/blocks/src/Title/Title.spec.tsx
@@ -1,10 +1,10 @@
 import { render } from '@testing-library/react';
 
-import { Flex } from './Flex';
+import { Title } from './Title';
 
-describe('Flex', () => {
+describe('Title', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<Flex />);
+    const { baseElement } = render(<Title>I'm a Title!</Title>);
     expect(baseElement).toBeTruthy();
   });
 });
